c2a256774c80c7f1f72d4eed63d04a88fc2be744
Franco Prudhomme <francoprud@gmail.com>
2015-11-15 17:34:58 -0300

    Adds .gitignore

--- src/main/java/edu/itba/pod/hazel/core/Reducer3.java
+++ src/main/java/edu/itba/pod/hazel/core/Reducer3.java
@@ -1,9 +1,7 @@
 package edu.itba.pod.hazel.core;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
