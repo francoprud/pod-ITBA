4554992c898ac5806e6abce776ed8d248114383f
Franco Prudhomme <francoprud@gmail.com>
2015-11-15 22:49:48 -0300

    Adds benchmarking times

--- src/main/java/edu/itba/pod/hazel/tp/Main.java
+++ src/main/java/edu/itba/pod/hazel/tp/Main.java
@@ -1,6 +1,7 @@
 package edu.itba.pod.hazel.tp;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.ExecutionException;
 
 import com.hazelcast.client.HazelcastClient;
@@ -17,18 +18,26 @@ import edu.itba.pod.hazel.parser.Parser;
 public class Main {
 	private static final String NAME = "group";
 	private static final String PASSWORD = "group";
-	
 
 	public static void main(String[] args) throws IOException,
 			InterruptedException, ExecutionException {
+		long start_reading_file = 0;
+		long end_reading_file = 0;
+		long start_query_run = 0;
+		long end_query_run = 0;
+		
 		HazelcastInstance client = setHazlecastConfiguration(NAME, PASSWORD);
 
 		try {
 			String path = System.getProperty("path");
+			
+			start_reading_file = System.currentTimeMillis(); 		// Metrics Purpose
 			Movie[] movies = new Parser(path).parseMovieJson();
-
+			end_reading_file = System.currentTimeMillis();			// Metrics Purpose
+			
 			int query = Integer.parseInt(System.getProperty("query"));
-
+			
+			start_query_run = System.currentTimeMillis();			// Metrics Purpose
 			switch (query) {
 			case 1:
 				new Query1(client, movies).run();
@@ -52,6 +61,19 @@ public class Main {
 		} catch (IOException e) {
 			System.out.println("The specified path is not valid.");
 		}
+		end_query_run = System.currentTimeMillis();					// Metrics Purpose
+		
+		printMetrics(start_reading_file, end_reading_file, start_query_run, end_query_run);
+	}
+
+	private static void printMetrics(long start_reading_file,
+			long end_reading_file, long start_query_run, long end_query_run) {
+		SimpleDateFormat date_parser = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:SSSS");
+		
+		System.out.println("Reading file start time: " + "\t" + date_parser.format(start_reading_file));
+		System.out.println("Reading file end time: " + "\t" + date_parser.format(end_reading_file));
+		System.out.println("MapReduce start time: " + "\t" + date_parser.format(start_query_run));
+		System.out.println("MapReduce end time: " + "\t" + date_parser.format(end_query_run));
 	}
 
 	private static HazelcastInstance setHazlecastConfiguration(String name,
